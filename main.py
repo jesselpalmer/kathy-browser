7aace422718a0af541d9d5db2a7a556866b24dc8
Jesse Palmer <jesselpalmer@gmail.com>
2022-02-20 17:14:24 -0800

    chore: add parsing response code

--- main.py
+++ main.py
@@ -1,20 +1,53 @@
+from re import I
 import socket
-s = socket.socket(
-    family=socket.AF_INET,
-    type=socket.SOCK_STREAM,
-    proto=socket.IPPROTO_TCP,
-)
+
+def is_valid_url(url):
+  return url.startswith('http://')
+
+def request(url):
+  s = socket.socket()
+
+  if not is_valid_url(url):
+    return None
+
+  url = url[len('http://'):]
+
+
+  host, path = url.split('/', 1)
+  path = '/' + path
+
+
+  s.connect((host, 80))
+  s.send(b'GET /index.html HTTP/1.0\r\n' + 
+        b'Host: example.org\r\n\r\n')
+
+  response = s.makefile("r", encoding="utf8", newline="\r\n")
+
+  statusline = response.readline()
+  version, status, explanation = statusline.split(" ", 2)
+  assert status == "200", "{}: {}".format(status, explanation)
+
+  headers = {}
+  while True:
+      line = response.readline()
+      if line == "\r\n": break
+      header, value = line.split(":", 1)
+      headers[header.lower()] = value.strip()
+
+  body = response.read()
+  s.close()
+
+  return headers, body
 
 url = 'http://example.org/'
+headers, body = request(url)
+print(headers, body)
 
-assert url.startswith('http://')
-url = url[len('http://'):]
-
-
-host, path = url.split('/', 1)
-path = '/' + path
-
-
-s.connect((host, 80))
-print(s.send(b"GET /index.html HTTP/1.0\r\n" + 
-       b"Host: example.org\r\n\r\n"))
+in_angle = False
+for c in body:
+    if c == '<':
+        in_angle = True
+    elif c == '>':
+        in_angle = False
+    elif not in_angle:
+        print(c, end='')
