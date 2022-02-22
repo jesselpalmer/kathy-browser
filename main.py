db8ee53ac51df54e2d808500445101a5973cbe57
Jesse Palmer <jesselpalmer@gmail.com>
2022-02-21 19:44:14 -0800

    refactor: lots

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import socket
 import sys
 
+HTTP_PORT = 80
+HTTPS_PORT = 443
+HTTP_SCHEME = 'http'
+HTTPS_SCHEME = 'https'
+URL_DELIMITER = '://'
+HTTP_BEGINNING = HTTP_SCHEME + URL_DELIMITER
+HTTPS_BEGINNING = HTTPS_SCHEME + URL_DELIMITER
+DEFAULT_CHAR_ENCODING = 'utf-8'
+EOL = '\r\n'
+OK_STATUS_CODE = '200'
+
 
 def is_valid_url(url):
-    return url.startswith('http://')
+    return url.startswith(HTTP_BEGINNING)
 
 
 def request(url):
@@ -12,30 +23,39 @@ def request(url):
     if not is_valid_url(url):
         return None
 
-    url = url[len('http://'):]
+    scheme, url = url.split(URL_DELIMITER, 1)
+    assert scheme in [HTTP_SCHEME, HTTPS_SCHEME], \
+        'Unknown scheme {}'.format(scheme)
 
     host, path = url.split('/', 1)
     path = '/' + path
-    host_string = f'Host: {host}\r\n\r\n'
-    host_string_bytes = bytes(host_string, encoding='utf-8')
-    s.connect((host, 80))
-    s.send(b'GET /index.html HTTP/1.0\r\n' +
-           host_string_bytes)
 
-    response = s.makefile("r", encoding="utf8", newline="\r\n")
+    port = HTTP_PORT if scheme == HTTP_SCHEME else HTTPS_PORT
+
+    s.connect((host, port))
+
+    http_request_string = f'GET /index.html HTTP/1.0{EOL}'
+    http_request_string_bytes = bytes(http_request_string, encoding=DEFAULT_CHAR_ENCODING)
+
+    host_string = f'Host: {host}{EOL}{EOL}'
+    host_string_bytes = bytes(host_string, encoding=DEFAULT_CHAR_ENCODING)
+
+    s.send(http_request_string_bytes + host_string_bytes)
+
+    response = s.makefile('r', encoding=DEFAULT_CHAR_ENCODING, newline=EOL)
 
     status_line = response.readline()
-    version, status, explanation = status_line.split(" ", 2)
-    assert status == "200", "{}: {}".format(status, explanation)
+    version, status, explanation = status_line.split(' ', 2)
+    assert status == OK_STATUS_CODE, '{}: {}'.format(status, explanation)
 
     headers = {}
     while True:
         line = response.readline()
 
-        if line == "\r\n":
+        if line == EOL:
             break
 
-        header, value = line.split(":", 1)
+        header, value = line.split(':', 1)
         headers[header.lower()] = value.strip()
 
     body = response.read()
@@ -46,7 +66,7 @@ def request(url):
 
 def show(body):
     in_angle = False
-    
+
     for c in body:
         if c == '<':
             in_angle = True
@@ -61,9 +81,12 @@ def load(url):
     show(body)
 
 
-if __name__ == "__main__":
-    if len(sys.argv) >= 1:
-        url = sys.argv[1]
+if __name__ == '__main__':
+    default_site = 'http://example.org/'
+
+    if len(sys.argv) >= 2:
+        site = sys.argv[1]
     else:
-        url = 'http://example.org/'
-    load(url)
+        site = default_site
+
+    load(site)
